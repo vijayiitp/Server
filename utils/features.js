8502bccc82a2c352b87dc577483c1d2bfbe857ed
Vijay Kumar <139329029+vijayiitp@users.noreply.github.com>
2024-10-19 20:01:52 +0530

    Update features.js

--- utils/features.js
+++ utils/features.js
@@ -1,83 +1,4 @@
 
-// import mongoose from "mongoose";
-// import jwt from "jsonwebtoken";
-// import { v4 as uuid } from "uuid";
-// import { v2 as cloudinary } from "cloudinary";
-// import { getBase64, getSockets } from "../lib/helper.js";
-
-// const cookieOptions = {
-//   maxAge: 15 * 24 * 60 * 60 * 1000,
-//   sameSite: "none",
-//   httpOnly: true,
-//   secure: true,
-// };
-
-// const connectDB = (uri) => {
-//   mongoose
-//     .connect(uri, { dbName: "Chattu" })
-//     .then((data) => console.log(`Connected to DB: ${data.connection.host}`))
-//     .catch((err) => {
-//       throw err;
-//     });
-// };
-
-// const sendToken = (res, user, code, message) => {
-//   const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-//   return res.status(code).cookie("chattu-token", token, cookieOptions).json({
-//     success: true,
-//     user,
-//     message,
-//   });
-// };
-// const emitEvent = (req, event, users, data) => {
-//   console.log("emit".event)
-//   // const io = req.app.get("io");
-//   // const usersSocket = getSockets(users);
-//   // io.to(usersSocket).emit(event, data);
-// };
-
-// const uploadFilesToCloudinary = async (files = []) => {
-//   const uploadPromises = files.map((file) => {
-//     return new Promise((resolve, reject) => {
-//       cloudinary.uploader.upload(
-//         getBase64(file),
-//         {
-//           resource_type: "auto",
-//           public_id: uuid(),
-//         },
-//         (error, result) => {
-//           if (error) return reject(error);
-//           resolve(result);
-//         }
-//       );
-//     });
-//   });
-
-//   try {
-//     const results = await Promise.all(uploadPromises);
-
-//     const formattedResults = results.map((result) => ({
-//       public_id: result.public_id,
-//       url: result.secure_url,
-//     }));
-//     return formattedResults;
-//   } catch (err) {
-//     throw new Error("Error uploading files to cloudinary", err);
-//   }
-// };
-
-// const deletFilesFromCloudinary = async (public_ids) => {
-//   // Delete files from cloudinary
-// };
-
-// export {
-//   connectDB,
-//   sendToken,
-//   cookieOptions,
-//   emitEvent,
-//   deletFilesFromCloudinary,
-//   uploadFilesToCloudinary,
-// };
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 import { v4 as uuid } from "uuid";
@@ -96,25 +17,20 @@ const connectDB = (uri) => {
     .connect(uri, { dbName: "Chattu" })
     .then((data) => console.log(`Connected to DB: ${data.connection.host}`))
     .catch((err) => {
-      console.error("Database connection error:", err);
-      throw err; // Consider handling this differently based on your application's needs
+      throw err;
     });
 };
 
 const sendToken = (res, user, code, message) => {
   const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-  return res
-    .status(code)
-    .cookie("chattu-token", token, cookieOptions)
-    .json({
-      success: true,
-      user,
-      message,
-    });
+  return res.status(code).cookie("chattu-token", token, cookieOptions).json({
+    success: true,
+    user,
+    message,
+  });
 };
-
 const emitEvent = (req, event, users, data) => {
-  console.log("Emitting event:", event);
+  console.log("emit".event)
   // const io = req.app.get("io");
   // const usersSocket = getSockets(users);
   // io.to(usersSocket).emit(event, data);
@@ -146,14 +62,12 @@ const uploadFilesToCloudinary = async (files = []) => {
     }));
     return formattedResults;
   } catch (err) {
-    console.error("Error uploading files to Cloudinary:", err);
-    throw new Error("Error uploading files to Cloudinary");
+    throw new Error("Error uploading files to cloudinary", err);
   }
 };
 
 const deletFilesFromCloudinary = async (public_ids) => {
-  // TODO: Implement the logic to delete files from Cloudinary
-  console.log("Deleting files from Cloudinary with public IDs:", public_ids);
+  // Delete files from cloudinary
 };
 
 export {
@@ -164,4 +78,3 @@ export {
   deletFilesFromCloudinary,
   uploadFilesToCloudinary,
 };
-
