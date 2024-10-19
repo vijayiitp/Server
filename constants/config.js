918e677dc0d3da762dc465a23af531a2d9ea4e72
Vijay Kumar <139329029+vijayiitp@users.noreply.github.com>
2024-10-20 01:28:21 +0530

    Update config.js

--- constants/config.js
+++ constants/config.js
@@ -2,7 +2,7 @@ const corsOptions = {
     origin: [
       "http://localhost:5173",
       "http://localhost:4173",
-      process.env.CLIENT_URL || "https://chatus-gd9f.vercel.app/" ,
+      process.env.CLIENT_URL || "https://chatus-gd9f-qmp3vw9nn-vijayiitps-projects.vercel.app" ,
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
